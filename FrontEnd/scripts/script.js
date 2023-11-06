6649ff805e3d5a6cb097f1abb9b6fc59714d3358
JulienR66 <julien-rodriguez66@outlook.fr>
2023-11-06 16:09:22 +0100

    Modif JS + css et création des filtres

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -1,11 +1,15 @@
 const reponse = await fetch("http://localhost:5678/api/works");
 const travaux = await reponse.json();
+genererTravaux(travaux);
 
-function genererTravaux () {
-    for (let i = 0; i < travaux.length; i++) {
+const buttons = document.querySelectorAll('.btn-filter');
 
-        const article = travaux[i];
-        const sectionTravaux = document.querySelector(".gallery");
+function genererTravaux(data) {
+    const sectionTravaux = document.querySelector(".gallery");
+    sectionTravaux.innerHTML = ""; // Pour effacer le contenu existant
+
+    for (let i = 0; i < data.length; i++) {
+        const article = data[i];
 
         const travauxContenant = document.createElement("figure");
 
@@ -13,15 +17,74 @@ function genererTravaux () {
         imageTravaux.src = article.imageUrl;
 
         const titreTravaux = document.createElement("figcaption");
-        titreTravaux.innerText = article.title
-
-        sectionTravaux.appendChild(travauxContenant);
+        titreTravaux.innerText = article.title;
 
         travauxContenant.appendChild(imageTravaux);
         travauxContenant.appendChild(titreTravaux);
 
+        sectionTravaux.appendChild(travauxContenant);
     }
 }
-genererTravaux()
 
-console.log(travaux)
+function deselectionnerTousLesBoutons() {
+    buttons.forEach(button => {
+        button.classList.remove('selected');
+    });
+}
+
+// const boutonTous = document.getElementById("btnTous");
+// boutonTous.addEventListener("click", function () {
+//     genererTravaux(travaux);
+//     deselectionnerTousLesBoutons(); // Désélectionner tous les boutons
+//     button.classList.add('selected'); // Appliquer la classe 'selected' au bouton cliqué
+// });
+
+// const boutonObjets = document.getElementById("btnObjets");
+// boutonObjets.addEventListener("click", function () {
+//     const categorieObjets = travaux.filter(travail => travail.categoryId === 1);
+//     genererTravaux(categorieObjets);
+//     deselectionnerTousLesBoutons(); // Désélectionner tous les boutons
+//     button.classList.add('selected'); // Appliquer la classe 'selected' au bouton cliqué
+// });
+
+// const boutonAppartements = document.getElementById("btnAppartements");
+// boutonAppartements.addEventListener("click", function () {
+//     const categorieAppartements = travaux.filter(travail => travail.categoryId === 2);
+//     genererTravaux(categorieAppartements);
+//     deselectionnerTousLesBoutons(); // Désélectionner tous les boutons
+//     button.classList.add('selected'); // Appliquer la classe 'selected' au bouton cliqué
+// })
+
+// const boutonHotelRestaurants = document.getElementById("btnHotelRestaurants");
+// boutonHotelRestaurants.addEventListener("click", function () {
+//     const categorieHotelRestaurants = travaux.filter(travail => travail.categoryId === 3);
+//     genererTravaux(categorieHotelRestaurants);
+//     deselectionnerTousLesBoutons(); // Désélectionner tous les boutons
+//     button.classList.add('selected'); // Appliquer la classe 'selected' au bouton cliqué
+// })
+
+
+
+
+
+
+buttons.forEach(button => {
+    button.addEventListener("click", function () {
+        deselectionnerTousLesBoutons(); // Désélectionner tous les boutons
+        button.classList.add('selected'); // Appliquer la classe 'selected' au bouton cliqué
+
+        if (button.id === 'btnTous') {
+            genererTravaux(travaux); // Afficher tous les projets
+        } else if (button.id === 'btnObjets') {
+            const categorieObjets = travaux.filter(travail => travail.categoryId === 1);
+            genererTravaux(categorieObjets); // Filtrer et afficher les projets de la catégorie 1
+        } else if (button.id === 'btnAppartements') {
+            const categorieAppartements = travaux.filter(travail => travail.categoryId === 2);
+            genererTravaux(categorieAppartements); // Filtrer et afficher les projets de la catégorie 2
+        } else if (button.id === 'btnHotelRestaurants') {
+            const categorieHotelRestaurants = travaux.filter(travail => travail.categoryId === 3);
+            genererTravaux(categorieHotelRestaurants); // Filtrer et afficher les projets de la catégorie 3
+        }
+        // Ajoutez des conditions pour les autres boutons si nécessaire
+    });
+});
